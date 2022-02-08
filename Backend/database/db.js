9e61faa1c9f74e0bef7baf05f99ece536fc3a447
Geepy <geepytechnologies@gmail.com>
2022-02-08 09:37:38 +0100

    created a new branch

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,4 +1,5 @@
 module.exports = {
     db: process.env.DATABASE_URL
-    // db: 'mongodb+srv://geepy:Richgift196897@cluster0.gzral.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    // db: 'mongodb+srv://geepy:Richgift196897@portfolio.gelcy.mongodb.net/contact?retryWrites=true&w=majority'
 };
+
