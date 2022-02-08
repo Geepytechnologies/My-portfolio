9726246e1334cb1f89092f775772c89146570cee
Geepy <geepytechnologies@gmail.com>
2022-02-08 10:34:45 +0100

    Update formRoute.js

--- Backend/routes/formRoute.js
+++ Backend/routes/formRoute.js
@@ -5,7 +5,8 @@ const router = express.Router()
 let formSchema = require('../models/form');
 
 
-router.post('/request', (req, res) => {
+router.post('/', (req, res) => {
+    res.send('Api request made')
     formSchema.create(req.body, (error, data) => {
       if (error) {
         //return next(error)
